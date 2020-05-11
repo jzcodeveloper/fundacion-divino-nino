586791268602cab1976312698b8821829ada7e17
Javier <javier_bislip@hotmail.com>
2020-05-11 04:53:55 -0400

    Updated index.html description

--- seeder.js
+++ seeder.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 // Read JSON files
